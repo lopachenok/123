f082970037dfbee18ea58fee9f43e01d090b2290
Lopachenok Sofya <Lopachenok.S@digdes.com>
2016-07-27 21:58:02 +0300

    rehab section carousel for mobile

--- src/blocks/carousel/carousel.js
+++ src/blocks/carousel/carousel.js
@@ -1,104 +1,125 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var carousel = document.getElementById('carousel');
-  var carouselContainer = document.getElementById('carousel-container');
-  var detecting, touch, x, y, started, newX, newY, newTouch, delta;
-  var screenWidth = parseInt(screen.width);
-
-  carousel.addEventListener("touchstart", function (e) {
-    if (e.touches.length != 1 || started) {
-      return;
-    }
-
-    detecting = true;
-    touch = e.changedTouches[0];
-    x = touch.pageX;
-    y = touch.pageY;
-  });
-
-  carousel.addEventListener("touchmove", function (e) {
-    if (!started && !detecting) {
-      return;
-    }
-
-    if (detecting) {
-      detect();
-    }
-
-    if (started) {
-      draw();
-    }
-    
-    newTouch = e.changedTouches[0];
-    newX = newTouch.pageX;
-    newY = newTouch.pageY;
-
-    function detect() {
-      var changesTouches = Array.prototype.slice.call(e.changedTouches);
-     
-//      if (changesTouches.indexOf(touch) == -1) {
-//        console.log(1)
-//        return;
-//      }
-
-      if (Math.abs(x - newX) >= Math.abs(y - newY)) {        
-        e.preventDefault();
-        started = true;
-      }
-
-      detecting = false;
-    }
-
-    function draw() {
-      
-      e.preventDefault();
-
-//      if (e.changedTouches.indexOf(touch) == -1) {
-//        return;
-//      }
-
-      
-      delta = x - newX;      
-      delta = -delta;
-           
-      if (delta < 0) {        
-        carouselContainer.style.transform = 'translateX('+(delta)+'px)';         
-      } else if (delta > 0) {
-        carouselContainer.style.transform = 'translateX('+(delta)+'px)';   
-      }
-
-    }
-
-  });
+  var carousel = document.getElementById("carousel");
+  var carouselContainer = document.getElementById("carousel-container");
+  var tabs = document.getElementById("tabs");
   
-  carousel.addEventListener("touchend", function() {  
-    var translate = carouselContainer.style.transform;
-    if(translate !== '') {
-      translate = translate.replace('translateX(', '').replace('px)', '');
-      translate = parseInt(translate);
-      console.log(translate)
-      if (delta < 0) {  
-         
-        if((0 >= translate) && (translate >= -screenWidth)) {
-          console.log(-screenWidth)
-          carouselContainer.style.transform = 'translateX('+(-screenWidth)+'px)';
+  tabs.addEventListener("click", function(e) {
+    e.preventDefault();
+    if(e.target.tagName !== "A") {
+      return;
+    }
+
+    HammerCarousel.prototype.show.apply(outer, [e.target.getAttribute("data-tab"), 0, true]);
+  });
+
+  var reqAnimationFrame = (function () {
+    return window[Hammer.prefixed(window, "requestAnimationFrame")] || function (callback) {
+      setTimeout(callback, 1000 / 60);
+    }
+  })();
+
+  function dirProp(direction, hProp, vProp) {
+    return (direction & Hammer.DIRECTION_HORIZONTAL) ? hProp : vProp
+  }
+
+
+  /**
+   * Carousel
+   * @param container
+   * @param direction
+   * @constructor
+   */
+  function HammerCarousel(container, direction) {
+    this.container = container;
+    this.direction = direction;
+
+    this.panes = Array.prototype.slice.call(this.container.children, 0);
+    this.containerSize = this.container[dirProp(direction, "offsetWidth", "offsetHeight")];
+
+    this.currentIndex = 1;
+
+    this.hammer = new Hammer.Manager(this.container);
+    this.hammer.add(new Hammer.Pan({
+      direction: this.direction,
+      threshold: 10
+    }));
+    this.hammer.on("panstart panmove panend pancancel", Hammer.bindFn(this.onPan, this));
+
+    this.show(this.currentIndex);
+  }
+
+
+  HammerCarousel.prototype = {
+    /**
+     * show a pane
+     * @param {Number} showIndex
+     * @param {Number} [percent] percentage visible
+     * @param {Boolean} [animate]
+     */
+    show: function (showIndex, percent, animate) {
+      showIndex = Math.max(0, Math.min(showIndex, this.panes.length - 1));
+      percent = percent || 0;
+      
+      document.querySelector(".carousel__tab--active").classList.remove("carousel__tab--active");
+      tabs.children[showIndex].classList.add("carousel__tab--active");
+      
+      if(showIndex === 0) {
+          tabs.style.transform = 'translate3d(9.5%, 0, 0)';
+      } else if(showIndex === 1) {
+        tabs.style.transform = 'translate3d(0, 0, 0)';  
+      } else if(showIndex === 2) {
+        tabs.style.transform = 'translate3d(-9.5%, 0, 0)';   
+      }
+     
+
+      var className = this.container.className;
+      if (animate) {
+        if (className.indexOf("animate") === -1) {
+          this.container.className += " animate";
         }
-        
-        if((translate >= 0) && (translate < screenWidth)) { 
-          carouselContainer.style.transform = 'translateX(0px)';
-        }
-      } else if (delta > 0) {
-        
-        if((0 <= translate) && (translate <= -screenWidth)) {
-          carouselContainer.style.transform = 'translateX(0px)';
-        }
-        
-        if((0 >= translate) && (translate >= -screenWidth)) {          
-          carouselContainer.style.transform = 'translateX('+(screenWidth)+'px)';
+      } else {
+        if (className.indexOf("animate") !== -1) {
+          this.container.className = className.replace("animate", "").trim();
         }
       }
-      
+
+      var paneIndex, pos, translate;
+      for (paneIndex = 0; paneIndex < this.panes.length; paneIndex++) {
+        pos = (this.containerSize / 100) * (((paneIndex - showIndex) * 100) + percent);
+        if (this.direction & Hammer.DIRECTION_HORIZONTAL) {
+          translate = "translate3d(" + pos + "px, 0, 0)";
+        } else {
+          translate = "translate3d(0, " + pos + "px, 0)"
+        }
+        this.panes[paneIndex].style.transform = translate;
+        this.panes[paneIndex].style.mozTransform = translate;
+        this.panes[paneIndex].style.webkitTransform = translate;
+      }
+
+      this.currentIndex = showIndex;
+    },
+
+    /**
+     * handle pan
+     * @param {Object} ev
+     */
+    onPan: function (ev) {
+      var delta = dirProp(this.direction, ev.deltaX, ev.deltaY);
+      var percent = (100 / this.containerSize) * delta;
+      var animate = false;
+
+      if (ev.type == "panend" || ev.type == "pancancel") {
+        if (Math.abs(percent) > 20 && ev.type == "panend") {
+          this.currentIndex += (percent < 0) ? 1 : -1;
+        }
+        percent = 0;
+        animate = true;
+      }
+
+      this.show(this.currentIndex, percent, animate);
     }
-    
-      
-  });
+  };
+
+  // the horizontal pane scroller
+  var outer = new HammerCarousel(carouselContainer, Hammer.DIRECTION_HORIZONTAL);
 });
---
+++ src/js/vendor.js
@@ -0,0 +1,1 @@
+//=../libs/hammer.min.js
