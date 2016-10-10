e720955ca3dbb06f9979763241089e0b798afbaf
lopachenok <s.lopachenok@andersenlab.com>
2016-10-10 21:25:14 +0300

    fix close

--- build/js/main.js
+++ build/js/main.js
@@ -522,30 +522,30 @@ document.addEventListener("DOMContentLoaded", function() {
     } else {
       el.addEventListener("click", openPopup);
     }
-    
+
   });
-  
+
   Array.prototype.forEach.call(overlays, function(overlay) {
     overlay.addEventListener("click", closePopup);
   });
-  
+
   popupOverlay.children[0].addEventListener("scroll", function(e) {
     scrollPosition = this.scrollTop;
   });
-  
+
   var closers = document.querySelectorAll(".popups__closer");
-  
+
   Array.prototype.forEach.call(closers, function(closer){
-    closer.addEventListener("click", closePopup);
+    closer.addEventListener("click", closeOnClick);
   });
 
-  
+
   if(window.innerWidth < desctopSize) {
     mainElem = document.getElementById("wrapper");
   } else {
     mainElem = document.body || document.documentElement;
   }
-  
+
   var windowSizePopup = {
     x: window.innerWidth,
     y: window.innerHeight
@@ -559,18 +559,18 @@ document.addEventListener("DOMContentLoaded", function() {
         mainElem = document.body || document.documentElement;
       }
     }
-    
+
     if(windowSizePopup.x < tabletSize && window.innerWidth > tabletSize && popups.currentIndex === 2) {
-      closePopup();
+      closeOnClick()
     }
-    
+
     if(windowSizePopup.x < desctopSize && window.innerWidth > desctopSize && popups.currentIndex === 0) {
-      closePopup();
+      closeOnClick();
     }
-    
+
     windowSizePopup.x = window.innerWidth;
   });
-  
+
 });
 var popupOverlay;
 var scrollTopOffset;
@@ -583,15 +583,18 @@ function closePopup(e) {
   if(e.target !== this) {
     return false;
   }
-  if(flag === false) {
-      mainElem.classList.remove("ov-hidden");
-    }    
-    document.documentElement.scrollTop = scrollTopOffset;
-    document.body.scrollTop = scrollTopOffset;
-    popupOverlay.classList.remove("popups-overlay--open");  
-    popupContact.classList.remove("popups-overlay--open");  
+  closeOnClick();
 }
 
+function closeOnClick() {
+  if(flag === false) {
+      mainElem.classList.remove("ov-hidden");
+  }
+  document.documentElement.scrollTop = scrollTopOffset;
+  document.body.scrollTop = scrollTopOffset;
+  popupOverlay.classList.remove("popups-overlay--open");
+  popupContact.classList.remove("popups-overlay--open");
+}
 
 function openPopup(e) {
   if(e.target.offsetParent.id.indexOf("popup") !== -1) {
@@ -603,34 +606,34 @@ function openPopup(e) {
   var popupNumber = btn.getAttribute("data-popups");
   popupOverlay.classList.add("no-animate");
   HammerCarousel.prototype.show.apply(popups, [popupNumber, 0, true]);
-  
+
   if(popupNumber == 1) {
     donateCount = btn.getAttribute("data-count");
     donatePeriod = btn.getAttribute("data-period");
-    
+
     if(donateCount) {
       summ.value = donateCount;
     }
-    
+
     if(donatePeriod) {
       selectOption(formDropdown.querySelector("*[data-value='"+donatePeriod+"']"));
     }
-    
+
     var substr;
     if(window.innerWidth < tabletSize) {
       substr = "/мес";
     } else {
       substr = " в месяц";
     }
-    
+
     if(donatePeriod && donateCount) {
       finalCount.innerHTML = donateCount+'&#8381;'+(donatePeriod == 'monthly'?substr:'');
     }
-    
+
   }
-  
+
   scrollTopOffset = document.body.scrollTop || document.documentElement.scrollTop;
-  mainElem.classList.add("ov-hidden"); 
+  mainElem.classList.add("ov-hidden");
   popupOverlay.classList.add("popups-overlay--open");
   popupOverlay.classList.remove("no-animate");
 }
@@ -640,7 +643,7 @@ function openContactsPopup(e) {
     openPopup(e);
   } else {
     popupContact.classList.add("popups-overlay--open");
-    mainElem.classList.add("ov-hidden"); 
+    mainElem.classList.add("ov-hidden");
     scrollTopOffset = document.body.scrollTop || document.documentElement.scrollTop;
   }
 }
